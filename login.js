cefe3a8176796aec61337a786f1a7f8b82be21e7
jainprishita <prishitajain80@gmail.com>
2019-10-20 19:24:34 +0530

    V1

---
+++ login.js
@@ -0,0 +1,36 @@
+var userRegistered=[];
+var loggedUser=[];
+var flag=0;
+function storeLoggedUser()
+{
+    sessionStorage.loggedUsers=JSON.stringify(loggedUser);
+}
+
+function getRegisteredUser()
+{
+    if(!localStorage.users)
+        {
+            localStorage.users=JSON.stringify([]);
+        }
+    else
+        {
+            userRegistered=JSON.parse(localStorage.users);
+        }
+}
+function matchValues()
+{
+  var uname=document.getElementById("uname").value;
+  var password=document.getElementById("password").value;
+  for(i=0;i<userRegistered.length;i++)
+      {
+          flag=0;
+          if(userRegistered[i].uname==uname && userRegistered[i].password==password)
+              {
+                  alert("Logged In");
+              }
+      }
+    if(flag==0)
+        {
+            alert("Invalid username or password");
+        }
+}
